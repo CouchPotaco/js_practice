82cdb796d63cdaaf9762b319dd49b039c638af49
m.shimamura <m.shimamura@ae.auaone-net.jp>
2022-11-09 16:59:57 +0900

    フロント06課題

---
+++ front_06/kadai.js
@@ -0,0 +1,14 @@
+//front06
+let fruit = ['apricot', 'strawberry'];
+
+// 配列の先頭に追加
+fruit.unshift('pear');
+// 配列の末尾に追加
+fruit.push('grape');
+
+//console.log(fruit);  
+
+// 最後のデータを'lime'に書き換える
+fruit[3] = 'lime';
+
+console.log(fruit);  
