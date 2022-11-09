9d7071703b11d573b115636165756b4ec5491892
m.shimamura <m.shimamura@ae.auaone-net.jp>
2022-11-09 17:38:11 +0900

    フロント課題07

---
+++ front_07/kadai.js
@@ -0,0 +1,15 @@
+//front06
+let fruits = { apple:'りんご', strawberry:'いちご', grape:'ぶどう', lemon:'れもん' };
+
+//「キー」grapeの値を出力
+console.log(fruits['grape']);
+
+//連想配列名fruitsのキーの一覧を出力
+for (key in fruits) {
+    console.log(key);
+}
+
+//連想配列名fruitsの全ての値を出力
+for (key in fruits) {
+    console.log(fruits[key]);
+}
